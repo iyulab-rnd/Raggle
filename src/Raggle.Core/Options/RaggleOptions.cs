4429cea27b28fd56468dadd3061b52e4e84234eb
junehyung <jhcj910805@gmail.com>
2024-06-26 11:11:35 +0900

    change

--- src/Raggle.Core/Options/RaggleOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Raggle.Core.Options;
-
-public class RaggleOptions
-{
-    public const string PROPERTY_NAME = "Raggle";
-}
--- src/Raggle.Core/Options/Vector/FileVectorOption.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Raggle.Core.Options.Vector;
-
-public class FileVectorOption
-{
-    public required string Directory { get; set; }
-}
